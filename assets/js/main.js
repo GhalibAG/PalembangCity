d2da474b45b52c8ab2ed46cb44ca09e949711993
GhalibAG <ghalibgtx@gmail.com>
2025-03-17 10:53:47 +0700

    First Doing

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        if ($(this).scrollTop() > $nav.height()) {
+           $nav.addClass("scrolled");
+           $nav.removeClass("navbar-light");
+           $nav.addClass("navbar-dark");
+        }
+        else {
+           $nav.removeClass("scrolled");
+              $nav.removeClass("navbar-dark");
+                $nav.addClass("navbar-light");
+        }
+    });
+});
